2da5c45c32c04dae0ff767fc7a62b181e4f5bef8
VictorZK <viethoa1996@gmail.com>
2019-12-02 17:43:54 +0200

    Added OneToOneRelationship

---
+++ MusicStore1/Models/ArtistDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore1.Models
+{
+    public class ArtistDetail
+    {
+        [Key()]
+        [ForeignKey("Artist")]
+        public int ArtistID { get; set; }
+        public string BIO { get; set; }
+        public virtual Artist Artist { get; set; }
+    }
+}
