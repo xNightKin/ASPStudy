1cc82acbaeadf2f7befe0e3c26d1fb42175c21fa
VictorZK <viethoa1996@gmail.com>
2019-12-03 12:01:30 +0200

    AddedInheritance

---
+++ MusicStore1/Models/SoloArtist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore1.Models
+{
+    public class SoloArtist : Artist 
+    {
+        public string Instrument { get; set; }
+    }
+}
